0d057bf216a47566e13c34348a295b7b8229448a
Rachel Stevenson <rachel@rachelrockaway.com>
2015-03-24 20:11:16 -0700

    Added sliders

--- RGB Sliders/ViewController.swift
+++ RGB Sliders/ViewController.swift
@@ -11,8 +11,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateBackgroudColor()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -21,6 +27,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func updateBackgroudColor() {
+        let red = CGFloat(redSlider.value)
+        let green = CGFloat(greenSlider.value)
+        let blue = CGFloat(blueSlider.value)
+        
+        view.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
+    }
 
 }
 
