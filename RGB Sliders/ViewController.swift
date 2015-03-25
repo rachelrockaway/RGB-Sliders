939baa6a1fb823c5eb14ec385375ee015c07aa56
Rachel Stevenson <rachel@rachelrockaway.com>
2015-03-24 20:22:01 -0700

    Add color square

--- RGB Sliders/ViewController.swift
+++ RGB Sliders/ViewController.swift
@@ -14,10 +14,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var redSlider: UISlider!
     @IBOutlet weak var greenSlider: UISlider!
     @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var colorSquare: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        updateBackgroudColor()
+        colorSquare.layer.borderColor = UIColor.blackColor().CGColor
+        colorSquare.layer.borderWidth = 1
+
         
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -32,7 +35,7 @@ class ViewController: UIViewController {
         let green = CGFloat(greenSlider.value)
         let blue = CGFloat(blueSlider.value)
         
-        view.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        colorSquare.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
     }
 
 }
